626977ab0617e02074476d5d9d699940a6af46ef
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2020-09-09 21:37:13 -0600

    implementacion de la pagina

---
+++ js/ejecutar.js
@@ -0,0 +1,30 @@
+
+
+function mm1(){
+    
+    console.log("-> Modelo MM1");
+    try { 
+    
+
+    }
+    catch (err) {
+        console.log(err);
+      
+    }
+
+}
+
+function mmk(){
+    console.log("-> Modelo MMK ");
+    try { 
+    
+
+    }
+    catch (err) {
+        console.log(err);
+      
+    }
+   
+
+}
+
